88648d54bce1871db48f60a7561ddab5831e7636
Matei-Ciofu <mateiciofu@gmail.com>
2025-07-15 16:42:17 +0200

    debug

--- PasswordGen.py
+++ PasswordGen.py
@@ -8,7 +8,7 @@ while True:
     if (any(c.islower() for c in password)
             and any(c.isupper() for c in password)
             and sum(c.isdigit() for c in password) >= 3
-            and any(c in string.punctiation for c in password)
+            and any(c in string.punctuation for c in password)
     ):
         break
 print(password)
